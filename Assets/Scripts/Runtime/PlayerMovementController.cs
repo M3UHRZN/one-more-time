d857e5e101d4e53fbb1ef15bf037d29b68f61f8d
Metin Enes Ufuk <metinenes76@gmail.com>
2026-07-04 04:37:35 +0300

    feat: slide + momentum + slide-hop (issue #3)

--- Assets/Scripts/Runtime/PlayerMovementController.cs
+++ Assets/Scripts/Runtime/PlayerMovementController.cs
@@ -3,24 +3,25 @@ using UnityEngine.InputSystem;
 
 namespace OneMoreTime
 {
-    /// GDD §3.6 çekirdek: Rigidbody tabanlı koşu + zıplama (coyote/buffer/tok yerçekimi).
-    /// Slide = issue #3 (Task 5). Air strafe = issue #4. Kamera cilası = issue #5.
+    /// GDD §3.6: Rigidbody koşu + zıplama (#2) + slide + momentum korunumu + slide hop (#3).
+    /// Air strafe = issue #4. Kamera cilası = issue #5.
     [RequireComponent(typeof(Rigidbody))]
     [RequireComponent(typeof(CapsuleCollider))]
     public class PlayerMovementController : MonoBehaviour
     {
         [SerializeField] InputActionAsset inputAsset;
         [SerializeField] MovementConfig config = new MovementConfig();
-        [SerializeField] Transform cameraTransform;   // kamera-göreli hareket için
+        [SerializeField] Transform cameraTransform;
         [SerializeField] LayerMask groundMask = ~0;
 
         Rigidbody _rb;
         CapsuleCollider _capsule;
         InputActionMap _playerMap;
-        InputAction _move, _jump;
+        InputAction _move, _jump, _crouch;
         JumpGate _jumpGate;
 
         bool _grounded;
+        bool _sliding;
 
         void Awake()
         {
@@ -36,6 +37,7 @@ namespace OneMoreTime
             _playerMap = inputAsset.FindActionMap("Player", true);
             _move = _playerMap.FindAction("Move", true);
             _jump = _playerMap.FindAction("Jump", true);
+            _crouch = _playerMap.FindAction("Crouch", true);
             _jumpGate = new JumpGate(config.coyoteTime, config.jumpBuffer);
         }
 
@@ -51,7 +53,7 @@ namespace OneMoreTime
         void FixedUpdate()
         {
             float dt = Time.fixedDeltaTime;
-            _grounded = ProbeGround(out _);
+            _grounded = ProbeGround(out Vector3 groundNormal);
             _jumpGate.Tick(dt, _grounded);
 
             Vector2 mv = _move.ReadValue<Vector2>();
@@ -59,26 +61,52 @@ namespace OneMoreTime
 
             Vector3 v = _rb.linearVelocity;
             Vector3 horiz = new Vector3(v.x, 0f, v.z);
+            float speed = horiz.magnitude;
+            bool crouchHeld = _crouch.IsPressed();
 
-            if (_grounded)
+            if (!_sliding && _grounded && crouchHeld && speed > config.runSpeed * 0.5f)
+            {
+                float boosted = MovementMath.SlideStartSpeed(speed, config.runSpeed, config.slideBoost);
+                Vector3 dir0 = horiz.sqrMagnitude > 0.001f ? horiz.normalized : transform.forward;
+                horiz = dir0 * boosted;
+                _sliding = true;
+            }
+            else if (_sliding && (!crouchHeld || !_grounded || speed < config.slideMinSpeed))
+            {
+                _sliding = false;
+            }
+
+            if (_sliding)
+            {
+                Vector3 dir = horiz.sqrMagnitude > 0.001f ? horiz.normalized : transform.forward;
+                float slopeA = MovementMath.SlopeAccel(groundNormal, dir, config.gravity);
+                float newSpeed = horiz.magnitude + slopeA * dt - config.slideFriction * dt;
+                horiz = dir * Mathf.Max(0f, newSpeed);
+            }
+            else if (_grounded)
             {
                 Vector3 target = wish * config.runSpeed;
                 float accel = wish.sqrMagnitude > 0.01f ? config.groundAccel : config.groundFriction;
                 horiz = Vector3.MoveTowards(horiz, target, accel * dt);
             }
-            // havada: yatay momentum korunur (air strafe = #4)
 
             float vy = v.y;
-            if (_grounded && vy < 0f) vy = -2f;   // zemine yapış
+            if (_grounded && vy < 0f) vy = -2f;
 
+            Vector3 nextVel = new Vector3(horiz.x, vy, horiz.z);
             if (_jumpGate.TryConsumeJump())
-                vy = MovementMath.JumpVelocity(config.jumpHeight, config.gravity);
+            {
+                nextVel = MovementMath.ApplyJump(nextVel, MovementMath.JumpVelocity(config.jumpHeight, config.gravity));
+                _sliding = false;
+            }
             else
-                vy += config.gravity * dt;
+            {
+                nextVel.y += config.gravity * dt;
+            }
 
-            _rb.linearVelocity = new Vector3(horiz.x, vy, horiz.z);
+            _rb.linearVelocity = nextVel;
 
-            FaceMoveDirection(wish, dt);
+            if (!_sliding) FaceMoveDirection(wish, dt);
         }
 
         Vector3 CameraRelative(Vector2 mv)
@@ -97,7 +125,6 @@ namespace OneMoreTime
             _rb.MoveRotation(Quaternion.RotateTowards(_rb.rotation, target, config.turnSpeed * dt));
         }
 
-        /// Ayak altında zemin var mı; varsa normalini döndürür.
         bool ProbeGround(out Vector3 normal)
         {
             float r = _capsule.radius * 0.9f;
